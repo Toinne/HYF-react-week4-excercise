30e3aba96f2e5f85f4fb9c3c8c9e9430545fc6e7
sena <48647939+msenaavci@users.noreply.github.com>
2019-09-06 20:16:31 +0200

    Update TodoList.js

--- app/src/TodoList.js
+++ app/src/TodoList.js
@@ -15,18 +15,57 @@ class TodoList extends Component {
     }
 
     async componentDidMount() {
-        const todos = await Api.getTodos();
-        this.setState({
-            todos
-        })
+        this.refreshTodos();
     }
 
+    async refreshTodos() {
+        const todos = await Api.getTodos();
+
+        if (!this.props.category) {
+            this.setState({
+                todos
+            });
+            return;
+        }
+
+        const filteredTodos = todos.filter((todo) => todo.category === this.props.category);
+
+        this.setState({
+            todos: filteredTodos
+        });
+    }
+
+    onResolve = async (_id) => {
+        const todo = this.state.todos.find((todo) => todo._id === _id);
+        todo.done = !todo.done;
+
+        await Api.editTodo(_id, todo);
+
+        this.refreshTodos();
+    };
+
+    onEdit = async (id) => {
+        this.setState({
+            editTodoId: id
+        });
+    };
+    onDelete =async(id) => {
+        const deleted = await Api.deleteTodo(id);
+        const todos = await Api.getTodos();
+        this.setState ({...this.state,todos});
+    
+    }
     render() {
         const {
-            todos
+            todos,
+            editTodoId
         } = this.state;
 
-        const $todos = todos.map((todo) => <TodoItem key={todo._id} {...todo} />);
+        if (editTodoId) {
+            return <Redirect to={`/todos/${editTodoId}`}/>;
+        }
+
+        const $todos = todos.map((todo) => <TodoItem key={todo._id} {...todo} onEdit={this.onEdit} onResolve={this.onResolve} onDelete ={this.onDelete}/>);
 
         return (
             <section className="TodoList">
