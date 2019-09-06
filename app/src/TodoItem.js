241033c508005b7be3a562982e5c8d954d49d754
sena <48647939+msenaavci@users.noreply.github.com>
2019-09-06 20:15:11 +0200

    Update TodoItem.js

--- app/src/TodoItem.js
+++ app/src/TodoItem.js
@@ -13,12 +13,23 @@ export default ({ _id, description, onResolve, onEdit }) => {
         event.preventDefault();
         onEdit(_id);
     };
+     const onDeleteClick = (event) => {
+        event.preventDefault();
+        onDelete(_id);
+    };
+
+    const then = moment(deadline);
+    const now = moment();
+    const countdown = then.diff(now,'days');
 
     return (
-        <li className="TodoItem">
+        <li className="TodoItem" style={countdown<= 2 ? {backgroundColor : 'red'}: {backgroundColor: 'yellow'}}>
             <button onClick={onResolveClick} className="TodoItem--resolve">✓</button>
             <span>{description}</span>
+            <span>{deadline}</span>
             <button onClick={onEditClick} className="TodoItem--edit">Edit</button>
+            <button onClick={onDeleteClick} className="TodoItem--delete">Delete</button>
+
         </li>
     )
 }
